2b099036950a19fb6fe314f3929603c7cc3cd8d1
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-04-18 11:57:27 +0200

    Implemented feature to add a new task to localStorage with increasing ID for each task

--- script/app-functions.js
+++ script/app-functions.js
@@ -1,10 +1,17 @@
 import { Task } from './app-classes.js';
 
-export function getTasks() {
+export function isStored() {
   const tasks = localStorage.getItem('currentTasks');
 
   if (tasks == null) return false;
   else return true;
+}
+
+export function getTasks() {
+  const storedTasks = localStorage.getItem('currentTasks');
+  const allTasks = JSON.parse(storedTasks);
+
+  return allTasks;
 }
 
 export function isEmpty(fieldToCheck) {
@@ -13,26 +20,48 @@ export function isEmpty(fieldToCheck) {
 }
 
 export function postData(dataToPost) {
-  const tasks = getTasks();
   let id = 0;
+  const stored = isStored();
 
-  if (!tasks) {
-    id = 1;
-    const taskToPost = createNewTask(id, dataToPost);
-    localStorage.setItem('currentTasks', JSON.stringify(taskToPost));
+  if (!stored) {
+    let id = 1;
+    const taskToStore = createNewTask(stored, id, dataToPost);
+    localStorage.setItem('currentTasks', JSON.stringify(taskToStore));
   } else {
-    const allTasks = JSON.parse(tasks);
-    const lastElement = allTasks.slice(-1);
-    lastElement.forEach((element) => {
-      id = element.id + 1;
-      console.log(id);
-      // TODO send to function that takes array out from  localStorage, push the data in the array and store it back
-    });
+    const storedTask = getTasks();
+    const lastId = getLastTaskId(storedTask);
+    id = lastId + 1;
+    const taskToAdd = createNewTask(stored, id, dataToPost);
+    addTask(taskToAdd);
   }
 }
 
-function createNewTask(taskId, taskContent) {
+function createNewTask(isStored, taskId, taskContent) {
   let newTask = new Task(taskId, taskContent);
+  if (!isStored) {
+    newTask = [new Task(taskId, taskContent)];
+  }
 
   return newTask;
+}
+
+function getLastTaskId(taskArray) {
+  let lastTaskId = 0;
+  const objectValues = Object.values(taskArray);
+  const lastStoredTask = objectValues.filter((taskId) => {
+    return taskId.id == objectValues.length;
+  });
+
+  lastStoredTask.forEach((task) => {
+    lastTaskId = task.id;
+  });
+
+  return lastTaskId;
+}
+
+function addTask(taskToAdd) {
+  let actualTasks = getTasks();
+  actualTasks = [...actualTasks, taskToAdd];
+
+  localStorage.setItem('currentTasks', JSON.stringify(actualTasks));
 }
