56a85be33df936881f4332dc932c4c5ab59260c5
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-04-18 17:45:30 +0200

    Added variables reused in many files for eventListeners

--- script/app-classes.js
+++ script/app-classes.js
@@ -6,3 +6,6 @@ export class Task {
 }
 
 export const list = document.querySelector('.task-display__list');
+export const displaySection = document.querySelector('.task-display__section');
+export const formError = document.querySelector('.app-form__error');
+export const submitBtn = document.querySelector('.app-form__post-data-btn');
