f47e82245f21fffa09023bf38fb9f4d8a2cb281e
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-04-18 09:35:06 +0200

    Deleted fetch code as not required and modified some functions to only work with localStorage

--- script/app-functions.js
+++ script/app-functions.js
@@ -1,6 +1,38 @@
-export async function getTasks() {
-  const data = await fetch('../data/tasks.json');
-  const tasks = await data.json();
+import { Task } from './app-classes.js';
 
-  return tasks;
+export function getTasks() {
+  const tasks = localStorage.getItem('currentTasks');
+
+  if (tasks == null) return false;
+  else return true;
+}
+
+export function isEmpty(fieldToCheck) {
+  if (fieldToCheck.value == '') return false;
+  else return true;
+}
+
+export function postData(dataToPost) {
+  const tasks = getTasks();
+  let id = 0;
+
+  if (!tasks) {
+    id = 1;
+    const taskToPost = createNewTask(id, dataToPost);
+    localStorage.setItem('currentTasks', JSON.stringify(taskToPost));
+  } else {
+    const allTasks = JSON.parse(tasks);
+    const lastElement = allTasks.slice(-1);
+    lastElement.forEach((element) => {
+      id = element.id + 1;
+      console.log(id);
+      // TODO send to function that takes array out from  localStorage, push the data in the array and store it back
+    });
+  }
+}
+
+function createNewTask(taskId, taskContent) {
+  let newTask = new Task(taskId, taskContent);
+
+  return newTask;
 }
